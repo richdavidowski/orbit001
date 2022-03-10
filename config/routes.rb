9d2314110ae6d6c25f08677d773522584f88c33f
Richie Davidowski <richdavidowski@gmail.com>
2022-03-10 14:56:00 -0700

    add html-pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'html_page/home'
+  get 'html_page/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
---
+++ test/controllers/html_page_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class HtmlPageControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get html_page_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get html_page_help_url
+    assert_response :success
+  end
+
+end
