743cfb4d0a10c66a975d457895aae97602f925c0
Richie Davidowski <richdavidowski@gmail.com>
2022-03-18 13:17:34 -0600

    Add login and authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "Greetings Earth Beings"
-  end
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'html_page#home'
   get '/help', to: 'html_page#help'
   get '/about', to: 'html_page#about'
   get '/contact', to: 'html_page#contact'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
